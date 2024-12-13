e86d8e3c0bcacaaf61ca4b1766c6d78238aa05cc
AlekseyB86 <avbelov86@mail.ru>
2024-12-13 12:42:36 +0300

    поправил RedisConfig

--- src/main/java/faang/school/analytics/config/redis/RedisConfig.java
+++ src/main/java/faang/school/analytics/config/redis/RedisConfig.java
@@ -1,16 +1,12 @@
 package faang.school.analytics.config.redis;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import faang.school.analytics.listener.AbstractEventListener;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.util.List;
 
@@ -25,20 +21,6 @@ public class RedisConfig {
         RedisStandaloneConfiguration configuration =
                 new RedisStandaloneConfiguration(redisProperties.host(), redisProperties.port());
         return new JedisConnectionFactory(configuration);
-    }
-
-    @Bean
-    RedisTemplate<String, Object> redisTemplate (
-            JedisConnectionFactory jedisConnectionFactory,
-            ObjectMapper objectMapper
-    ) {
-        final RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(jedisConnectionFactory);
-        GenericJackson2JsonRedisSerializer valueSerializer = new GenericJackson2JsonRedisSerializer(objectMapper);
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(valueSerializer);
-        redisTemplate.afterPropertiesSet();
-        return redisTemplate;
     }
 
     @Bean
