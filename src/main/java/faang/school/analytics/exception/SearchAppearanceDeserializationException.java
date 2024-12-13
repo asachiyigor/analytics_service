9c8d5db2084b737c2935817097c70375f8fe80c8
Igor <asachiyigor@gmail.com>
2024-12-13 14:29:31 +0300

    Задача "Анализ отображения профиля пользователя в запросах с фильтрами" BJS2-41355
    add exception

---
+++ src/main/java/faang/school/analytics/exception/SearchAppearanceDeserializationException.java
@@ -0,0 +1,7 @@
+package faang.school.analytics.exception;
+
+public class SearchAppearanceDeserializationException extends RuntimeException {
+    public SearchAppearanceDeserializationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
--- src/main/java/faang/school/analytics/listener/SearchAppearanceEventListener.java
+++ src/main/java/faang/school/analytics/listener/SearchAppearanceEventListener.java
@@ -2,6 +2,7 @@ package faang.school.analytics.listener;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import faang.school.analytics.dto.SearchAppearanceEventDto;
+import faang.school.analytics.exception.SearchAppearanceDeserializationException;
 import faang.school.analytics.mapper.AnalyticsEventMapper;
 import faang.school.analytics.model.AnalyticsEvent;
 import faang.school.analytics.service.AnalyticsEventService;
@@ -26,8 +27,8 @@ public class SearchAppearanceEventListener implements MessageListener {
         try {
             inputEvent = new ObjectMapper().readValue(message.getBody(), SearchAppearanceEventDto.class);
         } catch (IOException e) {
-            log.error("Error deserializing JSON to object", e);
-            throw new RuntimeException("Error deserializing JSON to object", e);
+            log.error("Error deserializing JSON to SearchAppearanceEventDto", e);
+            throw new SearchAppearanceDeserializationException("Failed to deserialize search appearance event", e);
         }
         SearchAppearanceEventDto event = SearchAppearanceEventDto.builder()
                 .foundUserId(inputEvent.getFoundUserId())
